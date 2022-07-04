b1c6108c3541037571767dd9ca6011b64ce028f9
Shikha Saini <83726276+shikha72@users.noreply.github.com>
2022-07-04 21:19:20 +0530

    Time: 30 ms (62.78%), Space: 18.3 MB (13.38%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        
+        vector<int>left(n,1);
+        vector<int>right(n,1);
+        
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                left[i]=left[i-1]+1;
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1])
+                right[i]=right[i+1]+1;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(left[i]>=right[i])
+                continue;
+            else 
+                left[i]=right[i];
+                
+        }
+        
+        int candies=0;
+        for(auto it: left)
+            candies+=it;
+        
+        return candies;
+    }
+};
