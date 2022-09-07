daad259b9b5b623da96d54a9c96a70dd5bc68b4a
Shikha Saini <83726276+shikha72@users.noreply.github.com>
2022-09-07 19:29:27 +0530

    Time: 6 ms (20.98%), Space: 8.4 MB (39.64%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,26 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//MORRIS INORDER TRAVERSAL
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>nodes;
-        stack<TreeNode*>st;
-        
-        while(1){
-            if(root!=NULL){
-                st.push(root);
-                root=root->left;
+        vector<int>inorder;
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
             }
             else{
-                if(st.empty())
-                    break;
-                root=st.top();
-                st.pop();
-                nodes.push_back(root->val);
-                root=root->right;
+                TreeNode* prev=curr->left;
+                //find rightmost node of left subtree
+                while(prev->right!=NULL && prev->right!=curr)
+                    prev=prev->right;
+                
+                if(prev->right==curr){
+                    //means left subtree has been visited so move right
+                    inorder.push_back(curr->val);
+                    prev->right=NULL;//remove thread
+                    curr=curr->right;
+                }
+                else{
+                    //means we have to visit left subtree so move left
+                    prev->right=curr;//create thread
+                    curr=curr->left;
+                }
             }
         }
-        return nodes;
+        return inorder;
     }
 };
