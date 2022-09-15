04088af388d2085c7f72424bcfed2e2a4946b1df
Shikha Saini <83726276+shikha72@users.noreply.github.com>
2022-09-15 12:04:02 +0530

    Time: 154 ms (79.17%), Space: 33.8 MB (83.33%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -3,25 +3,17 @@ public:
     int longestConsecutive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
-        int len=0;
-        int n=nums.size(), cnt=1;
-        if(n<=0)
-            return 0;
-        if(n==1)
-            return 1;
-        
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]+1)
-                cnt++;
-            else if(nums[i]==nums[i-1])
-                continue;
-            else{
-                len=max(len,cnt);
-                cnt=1;
+        int len=0, cnt=0, i=0, n=nums.size();
+        while(i<n){
+            while(i+1<n && (nums[i+1]-nums[i]==1 || nums[i+1]==nums[i])){
+                if(nums[i+1]-nums[i]==1)
+                    cnt++;
+                i++;
             }
+            len=max(cnt+1, len);
+            cnt=0;
+            i++;
         }
-        
-        len=max(len,cnt);
-        return len;
+        return len;        
     }
 };
