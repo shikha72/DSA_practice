02ae24ae17c34e6e54793396cf2a3d07a0a0ae16
Shikha Saini <83726276+shikha72@users.noreply.github.com>
2022-11-16 18:54:14 +0530

    Time: 204 ms (12.88%), Space: 31.5 MB (33.47%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void dfs (int r, int c, vector<vector<bool>>&vis, vector<vector<int>>&grid, int x[], int y[]){
+        int m=grid.size(), n=grid[0].size();
+        
+        vis[r][c]=1;      
+        
+        for(int i=0;i<4;i++){
+            int nr=r+x[i];
+            int nc=c+y[i];
+            
+            if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc]==1){
+                dfs(nr, nc, vis, grid, x, y);
+            }
+        }
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size(), n=grid[0].size();
+        vector<vector<bool>>vis(m, vector<bool>(n, 0));
+        
+        int x[]={0, -1, 0, 1};
+        int y[]={-1, 0, 1, 0};
+        
+        //traversing first and last row
+        for(int j=0;j<n;j++){
+            if(!vis[0][j] && grid[0][j]==1)
+                dfs(0, j, vis, grid, x, y);
+            
+            if(!vis[m-1][j] && grid[m-1][j]==1)
+                dfs(m-1, j, vis, grid, x, y);
+        }
+        
+        //traversing first and last col
+        for(int i=0;i<m;i++){
+            if(!vis[i][0] && grid[i][0]==1)
+                dfs(i, 0, vis, grid, x, y);
+            
+            if(!vis[i][n-1] && grid[i][n-1]==1)
+                dfs(i, n-1, vis, grid, x, y);
+        }
+        
+        //traversing unvisited cells with value 1
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j] && grid[i][j]==1)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
